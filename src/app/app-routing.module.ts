bb52e42a675138a6840a4586493a8a8dd10e22c7
revanthgoli01 <revanthgoli01@gmail.com>
2022-04-20 18:48:36 +0530

    routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcademicDetailsComponent } from './academic-details/academic-details.component';
 import { AuthGuard } from "./auth/auth.guard";
+import { StudentCreateComponent } from './student-create/student-create.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'student-create', pathMatch: 'full'},
   { path: "auth",
     loadChildren: () => import("./auth/auth.module").then(x => x.AuthModule)
   },
-  { path: "student-create",
-    loadChildren: () => import("./student-create/student.module").then(x => x.StudentModule)
-  },
+  { path: "student-create", component: StudentCreateComponent},
   { path: "auth/login",
     loadChildren: () => import("./auth/login/login.component").then(x => x.LoginComponent)
   },
